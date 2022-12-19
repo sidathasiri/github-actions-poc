74d4d1ff3b23d1de3f5452f1beed7c77e402a89c
sidath <sidathasiri@gmail.com>
2022-12-19 15:37:25 +0530

    added mock for launchdarkly dependency

--- test/integration/index.test.ts
+++ test/integration/index.test.ts
@@ -1,6 +1,8 @@
 const request = require("supertest");
 import app from '../../src/server';
 
+jest.mock('launchdarkly-node-server-sdk');
+
 describe("testing health route", () => {
   it("GET /health - success", async () => {
     const { body } = await request(app).get("/health");
